31fa658ae8a24f06f7642e163ae04549f4d1dd2e
lingting.fu <lingting.fu@okcoin.com>
2018-12-25 20:28:38 +0800

    Add Swap Restful & WebSocket API SDK

--- okex-go-sdk-api/config.go
+++ okex-go-sdk-api/config.go
@@ -10,6 +10,10 @@ package okex
 type Config struct {
 	// Rest api endpoint url. eg: http://www.okex.com/
 	Endpoint string
+
+	// Rest websocket api endpoint url. eg: ws://192.168.80.113:10442/
+	WSEndpoint string
+
 	// The user's api key provided by OKEx.
 	ApiKey string
 	// The user's secret key provided by OKEx. The secret key used to sign your request data.
